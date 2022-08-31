513883aef717d33100c6ff68046e74a991d51c0d
Davidbalua <37275920+Davidbalua@users.noreply.github.com>
2022-08-31 12:13:39 +0300

    Delete Trafic data and simulation directory

--- Trafic data and simulation/main.py
+++
@@ -1,6 +0,0 @@
-from Gothaerplatz import Vissim
-
-path = "D:\\Digital Engineering\\Jobs\\Transport Planning\\to be delivered to marco\\GothaerPlatz for python\\GothaerPlatz.inpx"
-gothaerplatz = Vissim(path)
-
-
