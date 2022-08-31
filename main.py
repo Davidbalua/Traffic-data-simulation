67bca2a5e6d6014a7d41c45369d814e2fc8ee236
Davidbalua <37275920+Davidbalua@users.noreply.github.com>
2022-08-31 12:17:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from Gothaerplatz import Vissim
+
+path = "D:\\Digital Engineering\\Jobs\\Transport Planning\\to be delivered to marco\\GothaerPlatz for python\\GothaerPlatz.inpx"
+gothaerplatz = Vissim(path)
+
+
